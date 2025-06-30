03486ccccf58c547f5b7ace20a453eef2f08ad65
Luimoe05 <lmoreno00528@gmail.com>
2025-06-30 10:57:10 -0700

    created a footer component and the css for it

---
+++ kudosBoard-ui/src/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer>
+        <div className="footer-section">
+          <h3>KudosBoard @ 2025</h3>
+        </div>
+      </footer>
+    </>
+  );
+}
--- kudosBoard-ui/src/main.jsx
+++ kudosBoard-ui/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Navbar from "./Navbar/Navbar.jsx";
 import Boards from "./Boards/Boards.jsx";
+import Footer from "./Footer/Footer.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbar />
     <Boards />
+    <Footer />
   </StrictMode>
 );
