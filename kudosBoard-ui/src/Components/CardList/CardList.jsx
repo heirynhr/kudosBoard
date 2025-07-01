46964d8d9b7d0ac2b1f74f345700172828952273
Luimoe05 <lmoreno00528@gmail.com>
2025-07-01 11:35:36 -0700

    storing away

--- kudosBoard-ui/src/Components/CardList/CardList.jsx
+++ kudosBoard-ui/src/Components/CardList/CardList.jsx
@@ -1,36 +1,51 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import "./CardList.css";
 import Cards from "../Cards/Cards.jsx";
 
 export default function CardList() {
+  const { boardId } = useParams();
   const [cards, setCards] = useState([]);
 
   // Fetching initial data
-  useEffect(() => {
-    const fetchCards = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/cards");
-        setCards(response.data);
-        // setError(null);
-      } catch (err) {
-        setError("Error getting card information");
-      }
-    };
-    fetchCards();
-  }, []);
+  // useEffect(() => {
+  //   const fetchCards = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         `http://localhost:3000/cards/${boardId}`
+  //       );
+  //       setCards(response.data);
+  //       // setError(null);
+  //     } catch (err) {
+  //       setError("Error getting card information");
+  //     }
+  //   };
+  //   fetchCards();
+  // }, []);
+
+  setCards([
+    {
+      title: "THIS IS TITLE 1",
+      description: "a description",
+      img: "",
+      author: "AUTHOR",
+      likes: 6,
+    },
+  ]);
 
   return (
     <>
       <div className="card-container">
-        {cards.map((m) => (
+        {cards.map((c) => (
           // Looping through cards
           <Cards
-            key={m.id}
-            title={m.title}
-            imgPath={m.poster_path}
-            rating={m.vote_average}
+            title={c.title}
+            description={c.description}
+            gif={c.gif}
+            author={c.author}
+            likes={c.likes}
           />
         ))}
       </div>
--- kudosBoard-ui/src/main.jsx
+++ kudosBoard-ui/src/main.jsx
@@ -12,7 +12,7 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/cards" element={<CardPage />} />
+        <Route path="/cards/:boardId" element={<CardPage />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
