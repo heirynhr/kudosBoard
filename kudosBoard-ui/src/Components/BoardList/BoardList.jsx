c48ee8ce9aed1b63e78f09ef8ab4356bc6f7ece4
Luis-Angel Moreno <lmoreno00528@gmail.com>
2025-07-02 14:34:28 -0700

    Luistest (#12)
    * cardlist styling fixed
    
    * added the sorting integration with the frontend

--- kudosBoard-ui/src/Components/BoardList/BoardList.jsx
+++ kudosBoard-ui/src/Components/BoardList/BoardList.jsx
@@ -3,30 +3,51 @@ import axios from "axios";
 import Boards from "../Boards/Boards";
 import { useState, useEffect } from "react";
 
-export default function BoardList() {
+export default function BoardList({ sortOption }) {
   const [boardData, setBoardData] = useState([]);
 
   useEffect(() => {
     const fetchBoardData = async () => {
       try {
-        const { data } = await axios.get("http://localhost:3000/boards");
+        if (sortOption.toLowerCase() === "recent") {
+          const { data } = await axios.get(
+            `http://localhost:3000/boards?sort=${sortOption}`
+          );
+          setBoardData(data);
+        } else if (sortOption.toLowerCase() === "all") {
+          const { data } = await axios.get(`http://localhost:3000/boards`);
+          setBoardData(data);
+        } else if (sortOption === "Thank You") {
+          const { data } = await axios.get(
+            `http://localhost:3000/boards?category=thank-you`
+          );
+          setBoardData(data);
+        } else {
+          const { data } = await axios.get(
+            `http://localhost:3000/boards?category=${sortOption}`
+          );
+          setBoardData(data);
+        }
 
-        setBoardData(data);
-        console.log("THE BOARD DATA HAS RAN", data);
+        // console.log(sortOption);
       } catch (error) {
         console.log(error);
       }
     };
 
     fetchBoardData();
-  }, []);
+  }, [sortOption]);
+
+  // const handleSortOption = (sortOption) => {
+  //   console.log(sortOption);
+  // };
 
   return (
     <>
       <div className="board-list-container">
         {boardData.map((boards) => {
           return (
-            <div>
+            <div key={boards.boardId}>
               <Boards
                 id={boards.boardId}
                 image={boards.coverImg}
