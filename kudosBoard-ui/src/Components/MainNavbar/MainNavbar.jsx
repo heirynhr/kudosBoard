ebc9bf93f4ba296dc4cc471f050193c18973d05b
Luimoe05 <lmoreno00528@gmail.com>
2025-07-02 13:23:02 -0700

    added the sorting integration with the frontend

--- kudosBoard-ui/src/Components/MainNavbar/MainNavbar.jsx
+++ kudosBoard-ui/src/Components/MainNavbar/MainNavbar.jsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import "./MainNavbar.css";
 
-function MainNavbar({ onSendData }) {
+function MainNavbar({ onSendData, onSortOption }) {
   const sendModalStatusToParent = () => {
     //This sends the data up to the parents
     // setNewBoard(!newBoard);
     onSendData(true);
+  };
+
+  const handleSort = (event) => {
+    //THIS WILL GIVE YOU THE VALUE OR SORT OPTION OF THE BUTTON
+    const sortOption = event.target.textContent;
+
+    // const sortThank
+    if (event.target.textContent === "Thank You") {
+      console.log("Thank you was called");
+    }
+    onSortOption(sortOption);
   };
 
   return (
@@ -18,11 +29,21 @@ function MainNavbar({ onSendData }) {
             <span className="material-symbols-outlined">search</span>
           </div>
           <div className="sorting-area">
-            <button>All</button>
-            <button>Recent</button>
-            <button>Celebration</button>
-            <button>Thank You</button>
-            <button>Inspiration</button>
+            <button className="sorting-btn" onClick={handleSort}>
+              All
+            </button>
+            <button className="sorting-btn" onClick={handleSort}>
+              Recent
+            </button>
+            <button className="sorting-btn" onClick={handleSort}>
+              Celebration
+            </button>
+            <button className="sorting-btn" onClick={handleSort}>
+              Thank You
+            </button>
+            <button className="sorting-btn" onClick={handleSort}>
+              Inspiration
+            </button>
 
             <button onClick={sendModalStatusToParent}>
               Create a New Board
