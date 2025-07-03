aa074eaaa3ebfd8cc4fcb37a3f81425805a33318
Luimoe05 <lmoreno00528@gmail.com>
2025-07-03 10:28:04 -0700

    changed inline spacing

--- kudosBoard-ui/src/Components/BoardList/BoardList.jsx
+++ kudosBoard-ui/src/Components/BoardList/BoardList.jsx
@@ -28,7 +28,6 @@ export default function BoardList({ sortOption, searchInput }) {
           setDisplayedBoards(data);
         } else {
           const { data } = await axios.get(
-
             `http://localhost:3000/boards?category=${sortOption.toLowerCase()}`
           );
           setBoardData(data);
